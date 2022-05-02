45d7bc6a0cf7d4ef68f1244cfb3be30ba9406296
Diego Escalante Urrelo <diego@tag1.com>
2022-05-02 01:59:06 -0500

    drupal-yjs/1: Add custom composer commands

---
+++ web/sites/default/template.settings.local.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * Template for additional local settings for the project.
+ *
+ * You can copy this file as `settings.local.php` to include any
+ * additional settings you need.
+ */
